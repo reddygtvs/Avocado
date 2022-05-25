fe9358c1679e8df9bfd5958f2f6fe5c6f1126268
Anannya Mahapatra <anannya1947@gmail.com>
2022-05-25 23:14:09 +0530

    splash screen with bugs

--- app/src/main/java/com/example/customice/MainActivity.java
+++ app/src/main/java/com/example/customice/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.customice;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,17 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        next_btn = (Button) findViewById(R.id.next_button);
-        send_name = (EditText) findViewById(R.id.user_name_input);
+        //getSupportActionBar().hide();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        next_btn.setOnClickListener(new View.OnClickListener(){
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v){
-                String str = send_name.getText().toString();
-                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
-                intent.putExtra("msg_key",str);
-                startActivity(intent);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                finish();
             }
-        });
+        },2000);
+
     }
 }
+
+
+
+//        next_btn = (Button) findViewById(R.id.next_button);
+//send_name = (EditText) findViewById(R.id.user_name_input);
+
+//next_btn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                String str = send_name.getText().toString();
+//                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+//                intent.putExtra("msg_key",str);
+//                startActivity(intent);
+//            }
+//        });
--- app/src/main/java/com/example/customice/MainActivity2.java
+++ app/src/main/java/com/example/customice/MainActivity2.java
@@ -7,15 +7,15 @@ import android.widget.TextView;
 import android.widget.VideoView;
 
 public class MainActivity2 extends AppCompatActivity {
-    TextView rcv_name;
+//    TextView rcv_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        rcv_name = (TextView) findViewById(R.id.name_display);
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("msg_key");
-        rcv_name.setText("Welcome " + str);
+//        rcv_name = (TextView) findViewById(R.id.name_display);
+//        Intent intent = getIntent();
+//        String str = intent.getStringExtra("msg_key");
+//        rcv_name.setText("Welcome " + str);
     }
 }
